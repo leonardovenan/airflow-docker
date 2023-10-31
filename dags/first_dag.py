af824bb4d954ac2e238f8fa42cf62b524ee0b49c
leonardovenan <leonardovenancio12@gmail.com>
2023-10-31 17:01:08 -0300

    adicionando dags testes

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -4,13 +4,13 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id = "first_dag_v0",
+    dag_id="first_DAG_v0",
     start_date=datetime(2023, 6, 1),
-    schedule = "@daily",
-    doc_md =__doc__,
+    schedule="@daily",
+    doc_md=__doc__,
 ):
-    start = EmptyOperator(task_id= "start")
-    hello = BashOperator(task_id= "hello", bash_command = "echo hello world")
-    end = EmptyOperator(task_id = "end")
+    start = EmptyOperator(task_id="start")
+    hello = BashOperator(task_id="hello", bash_command="echo hello world")
+    end = EmptyOperator(task_id="end")
 
 start >> hello >> end
