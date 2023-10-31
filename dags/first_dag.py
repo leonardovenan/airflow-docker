50f26f537b2fc6623433d1950edd5b52f43d112f
leonardovenan <leonardovenancio12@gmail.com>
2023-10-31 16:46:37 -0300

    adicionando dags testes

---
+++ dags/first_dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id = "first_dag_v0",
+    start_date=datetime(2023, 6, 1),
+    schedule = "@daily",
+    doc_md =__doc__,
+):
+    start = EmptyOperator(task_id= "start")
+    hello = BashOperator(task_id= "hello", bash_command = "echo hello world")
+    end = EmptyOperator(task_id = "end")
+
+start >> hello >> end
---
+++ dags/git_pull.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+import pendulum
+
+local_tz = pendulum.timezone("America/Sao_Paulo")
+
+default_args = {
+    'owner': 'lattice',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 11, 14, 0, 0, 0, tzinfo=local_tz),
+    'email': ['leonardovenancio12@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 3,
+    'dagrun_timeout': timedelta(minutes=60),  # using 60 min as default
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG('airflow_hello', default_args=default_args, schedule_interval="*/5 * * * *", max_active_runs=1) #,on_failure_callback=sn.slack_failed_task)
+
+#Teste de Dags
+t9X01 = BashOperator(
+    task_id='airflow_hello_1',
+    bash_command="\"D:/Backup PC/Code/airflow-docker/hello.py\"",
+    dag=dag)
+
+t9X02 = BashOperator(
+    task_id='airflow_hello_2',
+    bash_command="\"D:/Backup PC/Code/airflow-docker/hello2.py\"",
+    dag=dag)
+
+t9X01 >> t9X02
