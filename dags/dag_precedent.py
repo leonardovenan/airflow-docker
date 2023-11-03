bd7eebe2ea5717d7a40641aee41c7a2172cdbf25
leonardovenan <leonardovenancio12@gmail.com>
2023-11-03 17:06:04 -0300

    precedentia dag

---
+++ dags/dag_precedent.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+dag = DAG('dag_precedencia', description="execução de dag_exemplo com precedencia",
+          schedule_interval=None, start_date=datetime(2023,3,5),
+          catchup=False)
+
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 5", dag=dag)
+
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5", dag=dag)
+
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", dag=dag)
+
+[task1, task2] >> task3
