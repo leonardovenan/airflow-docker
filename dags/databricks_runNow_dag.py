99cdc2898feba2e2e237dbf4d4fe127e15fe181b
leonardovenan <leonardovenancio12@gmail.com>
2023-11-07 18:32:55 -0300

    create dag to run job or workflows on databricks

---
+++ dags/databricks_runNow_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
+from airflow.providers.databricks.operators.databricks import (
+    DatabricksSubmitRunOperator,
+)
+from datetime import datetime
+
+default_args = {"owner": "aairflow-leo"}
+
+with DAG(
+    "databricks_runNow_dag",
+    start_date=datetime(2023, 11, 7),  # Substitua com a data desejada
+    schedule_interval="0 0 * * *",  # Executar diariamente à meia-noite
+    default_args=default_args,
+) as dag:
+    # rodar exemplos para teste
+    # rodar um notebook no repositório
+    run_existing_repo_job = DatabricksRunNowOperator(
+        task_id="run_existing_repo_job",
+        databricks_conn_id="databricks_default",
+        job_id=26713014130385,
+        dag=dag,
+    )
